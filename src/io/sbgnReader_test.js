c3698613bdbe64099036a003488282ca4f632c01
Duan Lian <blazeblue@gmail.com>
2012-04-30 14:59:57 +0200

    add logger window

--- src/io/sbgnReader_test.js
+++ src/io/sbgnReader_test.js
@@ -5,11 +5,11 @@ goog.require('sb.io.SbgnReader');
 goog.require('sb.io.sbgnReaderTest.data');
 
 
-function setUp(){
+function setUp() {
 
 }
 
-function testSbgnReader(){
-    var reader=new sb.io.SbgnReader();
+function testSbgnReader() {
+    var reader = new sb.io.SbgnReader();
     reader.parseXmlText(sb.io.sbgnReaderTest.data.sbgnXML1);
 }
--- src/io/xmlReader.js
+++ src/io/xmlReader.js
@@ -1,6 +1,15 @@
 goog.provide('sb.io.XmlReader');
 
 goog.require('goog.dom.xml');
+goog.require('goog.dom.NodeType');
+goog.require('goog.array');
+goog.require('goog.debug');
+goog.require('goog.debug.Logger');
+goog.require('goog.debug.FancyWindow');
+
+var logWindow=new goog.debug.FancyWindow('main');
+logWindow.setEnabled(true);
+logWindow.init();
 
 /**
  * Base class for xml-based format file reader
@@ -12,11 +21,34 @@ sb.io.XmlReader = function () {
 
 /**
  *
+ * @type {goog.debug.Logger}
+ * @protected
+ */
+sb.io.XmlReader.prototype.logger = goog.debug.Logger.getLogger('sb.io.XmlReader');
+
+/**
+ *
  * @param xmlText
  * @return {*}
  */
 sb.io.XmlReader.prototype.parseXmlText = function (xmlText) {
+    this.logger.info('Parsing xml size:' + xmlText.length);
     var document = goog.dom.xml.loadXml(xmlText);
-    console.log('document'+document);
+    var rootElement = document.documentElement;
+    this.traverse(rootElement);
     return document;
 };
+
+/**
+ *
+ * @param {Element} node
+ */
+sb.io.XmlReader.prototype.traverse = function (node) {
+    goog.array.forEach(node.childNodes, function (child) {
+        if (child.nodeType == goog.dom.NodeType.ELEMENT) {
+            this.traverse(child);
+        }
+    }, this);
+};
+
+
